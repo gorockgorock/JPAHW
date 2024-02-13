049ed408efd7a16dc570c3c7a478620a4d525f73
ksj58884 <ksj58884@gmail.com>
2024-02-13 13:57:42 +0900

    erse
    병합

--- src/main/java/com/sparta/travelnewsfeed/entity/Post.java
+++ src/main/java/com/sparta/travelnewsfeed/entity/Post.java
@@ -1,26 +1,5 @@
 package com.sparta.travelnewsfeed.entity;
 
-import jakarta.persistence.*;
-import lombok.Getter;
-import lombok.Setter;
-
-@Entity
-@Table(name = "travel_post")
-@Getter
-@Setter
 public class Post {
-
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long postid;
-
-    private String title;
-
-    @Lob
-    private String content;
-
-    @ManyToOne
-    @JoinColumn(name = "user_id")
-    private User user;
 
 }
